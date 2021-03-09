efda617a06e81f344b7a4c7ee50f0cff4ca0fe95
Samuel Tenschert <samuel.tenschert@gmail.com>
2021-03-09 13:39:36 +0100

    basic 1 done

--- Basic1/style.js
+++ Basic1/style.js
@@ -2,31 +2,48 @@
 var btns = document.getElementsByClassName("btn");
 
 console.log(btns)
-// function printValue() {
 
-// }
 
-for (let i = 0; i < btns.length; i++) {
-        btns[i].addEventListener("click", function(){
-            printValue(i)
-        })
+
+function printValue() {
+    let firstName = document.getElementById("fname").value;
+
+    let lastName = document.getElementById("lname").value;
+
+    let age = document.getElementById("age").value;
+
+    document.getElementById("output1").innerHTML = `${firstName} <p>${lastName} <p>${age}`;
+    
+    //.length counts the characters in a string which in our case is the value of "firstName".
+    let str = firstName;
+    let n = str.length;
+    // document.getElementById("output2").innerHTML = n; 
+
+
+    if (str.length > 5) {
+        document.getElementById("output1").style.color= "green";
     }
-
-
-function printValue(i) {
-    var firstName = document.getElementById("fname").value;
-
-    var lastName = document.getElementById("lname").value;
-
-    var age = document.getElementById("age").value;
-
-    document.getElementById("output1").innerHTML = firstName;
-    document.getElementById("output2").innerHTML = lastName;
-    document.getElementById("output3").innerHTML = age;
+    else {
+        document.getElementById("output1").style.color= "red";
+    }
 
 }
 
-// document.getElementsByClassName("btns").addEventListener("click", printValue);
+document.getElementsByClassName("btn")[0].addEventListener("click", printValue);
 
 
 
+
+
+
+
+
+
+
+
+// for (let i = 0; i < btns.length; i++) {
+//         btns[i].addEventListener("click", function(){
+//             printValue(i)
+//         })
+//     }
+// Did the loop because we made more buttons...
