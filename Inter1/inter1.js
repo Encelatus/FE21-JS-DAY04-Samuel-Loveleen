fe50b6bf21567665be7ef0c666d9c227308bd171
Samuel Tenschert <samuel.tenschert@gmail.com>
2021-03-09 20:19:47 +0100

    .

--- Inter1/inter1.js
+++ Inter1/inter1.js
@@ -3,14 +3,14 @@
 
 function addRemoveToGrid(){
     var santaC = document.getElementsByClassName("santa");
-
+    //  (let i in santaC){}
     for (let i = 0 ; i < santaC.length; i++){
         santaC[i].addEventListener("click",function(){
         
             this.style.opacity= "0";
         
             this.style.transition = "1s";
-            setTimeout(()=>{this.hidden();},1000);
+            setTimeout(()=>{this.remove();},1000);
             });
     }
     
